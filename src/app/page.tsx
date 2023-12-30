26bf37ee7003bf6265e96c394d0f91e8205f1689
Deskrel <ismaelpsy.12@gmail.com>
2023-12-30 16:14:23 -0500

    get products for main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,52 @@
-import { Card, CardBody, CardHeader, Heading, Image } from '@chakra-ui/react'
+import { Card, CardBody, CardFooter, CardHeader, Grid, GridItem, Heading, Image } from '@chakra-ui/react'
+import { fetchData } from '@/services/fetchData'
 
-export default function Home() {
+interface Product {
+  id: number
+  title: string
+  description: string
+  image_urls: string[]
+  tags: string[]
+  price: number
+  available: boolean
+  createdAt: Date
+  updatedAt: Date
+}
+
+async function loadProducts () {
+  const products = await fetchData("/api/products")
+  return products;
+}
+
+export default async function Home() {
+
+  const products = await loadProducts()
+
   return (
     <div className='container mx-auto bg-sky-500'>
-      <Heading as='h1' size='2xl' className='text-center'>Bienvenido a Boutique Belen</Heading>
-      <main className=''>
-    
+      <div className='bg-red-100 mb-2 rounded-md'>
+        <Heading as='h1' size='2xl' className='text-center h-min p-2'>
+          Bienvenid@ a Boutique Belen
+        </Heading>
+      </div>
+      <main className='bg-gray-200 min-h-[calc(100vh-50px)] rounded-md p-2'>
+        <Grid templateColumns={['repeat(2, 1fr)', 'repeat(3, 1fr)']} gap={[2,6]}>
+          {products.map((p: Product) =>
+            <GridItem key={p.id} >
+              <Card>
+                <CardHeader p={2}>
+                  <Heading size='xs' className=''>{p.title}</Heading>
+                </CardHeader>
+                <CardBody p={2}>
+                  <Image src={p.image_urls[0]} alt={p.title} />
+                </CardBody>
+                {/* <CardFooter p={2}>
+                  {p.price}
+                </CardFooter> */}
+              </Card>
+            </GridItem>  
+          )}
+        </Grid>
       </main>
     </div>
 
