28b76674ed3e794e5b54504be3205eaacc6bd520
Deskrel <ismaelpsy.12@gmail.com>
2024-01-01 14:29:23 -0500

    put metadatos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,17 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Boutique Belen | Home',
-  description: 'Store for baby clothes',
+  description: 'Ropa para el engreído del hogar, en los mejores modelos, con la mejor calidad y el mejor precio.',
+  openGraph: {
+    title: 'Boutique Belen | Home',
+    description: 'Ropa para el engreído del hogar, en los mejores modelos, con la mejor calidad y el mejor precio.',
+    url: 'https://bbelen.vercel.app/home',
+    siteName: 'Boutique Belen',
+    locale: 'es_ES',
+    type: 'website',
+    countryName: 'PE',
+    images: 'https://res.cloudinary.com/ddr6m52rr/image/upload/v1704137177/tsislb68rjgpfvshlq3d.png'
+  }
 }
 
 export default function RootLayout({
